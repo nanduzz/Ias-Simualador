72d01fc1102d09f049716bec4cf9c536b9bbcb67
nanduzz <fernanduzz@gmail.com>
2015-08-31 14:18:47 -0300

    codificador haxadecimal iniciado

--- src/src/Decodificador.java
+++ src/src/Decodificador.java
@@ -1,19 +1,23 @@
 package src;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Decodificador {
 	
 	private String[] linhas;
-	public List<Pair> palavrasEspeciais;
-	public boolean modoDados = true;
+	public List<Pair> palavrasEspeciais = new ArrayList<Decodificador.Pair>();
+	public boolean modoDados = false;
 	public boolean instL = false;
 	public static long Instrucao;
 	public static int MemPos;
+	private static final int PAD_LIMIT = 8192;
 
 	public void armazenaDados(String linha){
 		
@@ -30,14 +34,14 @@ public class Decodificador {
 	}
 	
 	public boolean linhaVazia(String linha){
-		return linha == "" || linha == null;
+		return linha == null || linha.trim().equals("");
 	}
 	
 	public boolean isPalavraEspacial(String linha){
-		return linha.contains(":");
+		return linha.contains(":") && !linha.contains("(");
 	}
 	
-	public void getLinhasFromFile(String filePath){
+	public void codificaArquivo(String filePath){
 		this.linhas = new String[1024];
 		Mem.initMEM(this.linhas.length);
 		try{
@@ -57,19 +61,26 @@ public class Decodificador {
 		}
 	}
 	
-	public void decodifica(){
+	private void setMemPosOrg(String linha){
+		MemPos = Integer.valueOf(linha.replaceAll("[^0-9]", ""));
+	}
+	public void codifica(){
 		MemPos = 0;
 		for(String linha : this.linhas){
-			if(hasOrg(linha)){
-				continue;
-			}else if(isPalavraEspacial(linha)){
-				palavrasEspeciais.add(new Pair(MemPos + 1, linha.trim()));
-				continue;
-			}else{
-				if(modoDados){
-					insereDados(linha);
+			if(!linhaVazia(linha)){
+				if(hasOrg(linha)){
+					setMemPosOrg(linha);
+					continue;
+				}else if(isPalavraEspacial(linha)){
+					System.out.println("palavra especial");
+					palavrasEspeciais.add(new Pair(MemPos, linha.trim()));
+					continue;
 				}else{
-					insereInstrucao(linha);
+					if(modoDados){
+						insereDados(linha);
+					}else{
+						insereInstrucao(linha);
+					}
 				}
 			}
 		}
@@ -78,7 +89,11 @@ public class Decodificador {
 	public void insereDados(String dados){
 		if(instL)
 			MemPos++;
-		Mem.writeMEM(MemPos, Long.valueOf(dados), 0l);
+		Long l = Long.valueOf(dados.trim());
+		if(l < 0){
+			dados = Long.toString((Long.valueOf("8000000000", 16) | -l));
+		}
+		Mem.writeMEM(MemPos, Long.valueOf(dados.trim()), 0l);
 		MemPos++;
 	}
 	
@@ -130,9 +145,9 @@ public class Decodificador {
 			opH = Long.valueOf("10100", 2);
 		}else if(op.contains("RSH")){
 			opH = Long.valueOf("10101", 2);
-		}else if(op.contains("STOR M")){
+		}else if(op.contains("STOR")){
 			opH = Long.valueOf("10010", 2);
-		}else if(op.contains("STOR M")){
+		}else if(op.contains("STOR")){
 			opH = Long.valueOf("10011", 2);
 		}
 		return opH;
@@ -157,23 +172,75 @@ public class Decodificador {
 		if(!instL){
 			Instrucao = inst << 20;
 			instL = true;
+			Mem.writeMEM(MemPos, Instrucao, 0);
 		}else{
 			Instrucao = Instrucao | inst; 
 			instL = false;
+			Mem.writeMEM(MemPos, Instrucao, 0);
+			Instrucao = 0;
 			MemPos++;
 		}
 	}
 	
+	public void comeca(String filePath){
+		codificaArquivo(filePath);
+		codifica();
+	}
 	public static void main(String[] args) {
 		Decodificador d = new Decodificador();
 		Scanner scan = new Scanner(System.in);
 		String s;
-		do{
-			s = scan.nextLine();
-			
-			System.out.println(d.instrucaoToHex(s).toString());
-			//System.out.println(d.posMemToHex(s).toString());
-		}while(s != "sair");
+//		do{
+//			s = scan.nextLine();
+//			
+//			System.out.println(d.instrucaoToHex(s).toString());
+//			//System.out.println(d.posMemToHex(s).toString());
+//		}while(!s.equals("sair"));
+		d.comeca("bublesort2.ias");
+		Util.printfMEM(200, 0, 200);
+		d.escreveArquivo();
+	}
+	
+	public void escreveArquivo(){
+		try{
+			BufferedWriter bw = new BufferedWriter(new FileWriter("c:\\teste.txt"));
+			String linha = "";
+			for(Long l : Mem.mem){
+				bw.append(leftPad(Long.toString(l, 16), 10, '0'));
+				//bw.append(Long.toString(l, 16));
+				bw.newLine();
+			}
+			bw.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private static String padding(int repeat, char padChar)
+			throws IndexOutOfBoundsException {
+		if (repeat < 0) {
+			throw new IndexOutOfBoundsException(
+					"Cannot pad a negative amount: " + repeat);
+		}
+		final char[] buf = new char[repeat];
+		for (int i = 0; i < buf.length; i++) {
+			buf[i] = padChar;
+		}
+		return new String(buf);
+	}
+	
+	public static String leftPad(String str, int size, char padChar) {
+		if (str == null) {
+			return null;
+		}
+		int pads = size - str.length();
+		if (pads <= 0) {
+			return str; // returns original String when possible
+		}
+		if (pads > PAD_LIMIT) {
+			return leftPad(str, size, (char)padChar);
+		}
+		return padding(pads, padChar).concat(str);
 	}
 	
 	class Pair{
